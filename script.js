b1a7391a0b9e6502c41ae70842f0c42fcf90f51d
Vitor Campos <vitorcampos742@gmail.com>
2021-10-25 17:32:37 -0300

    resultados obrigatorios do jeito que querem

--- script.js
+++ script.js
@@ -1,53 +1,56 @@
-let paint = document.querySelector('#pixel-board')
+const paint = document.querySelector('#pixel-board');
+let botao = document.createElement('button')
+botao.innerText = 'Limpar'
+botao.style.display = 'block'
+botao.id = 'clear-board'
+document.querySelector('#c1').appendChild(botao)
 
-for (i = 1; i <= 25; i++) {
-    let pix = document.createElement('div')
-    pix.className = 'pixel'
-    paint.appendChild(pix)
-    if (i % 5 == 0) {
-        let bar = document.createElement('br')
-        paint.appendChild(bar)
-    }
+for (let i = 1; i <= 25; i += 1) {
+  const pix = document.createElement('div');
+  pix.className = 'pixel';
+  paint.appendChild(pix);
+  if (i % 5 == 0) {
+    const bar = document.createElement('br');
+    paint.appendChild(bar)
+  }
 }
 
 function pincel(event) {
-    let atpin = document.querySelector('.selected')
-    atpin.className = 'color'
-    event.target.className = 'color selected'
+  const atpin = document.querySelector('.selected');
+  atpin.className = 'color'
+  event.target.className = 'color selected';
 }
 
 
 let cor = document.querySelectorAll('.color')
 for (i of cor) {
-    i.addEventListener('click', pincel)
+  i.addEventListener('click', pincel)
 }
 
 function colorir(e) {
-    let atpin = document.querySelector('.selected')
-    if (atpin == document.querySelector('#black')) {
-        e.target.style.background = 'black'
-    } else if (atpin == document.querySelector('#blue')) {
-        e.target.style.background = 'blue'
-    } else if (atpin == document.querySelector('#green')) {
-        e.target.style.background = 'green'
-    } else if (atpin == document.querySelector('#red')) {
-        e.target.style.background = 'red'
-    }
-    /* console.log(atpin) */
-    /* e.target.style.background = 'red' */
+  let atpin = document.querySelector('.selected')
+  if (atpin == document.querySelector('#black')) {
+    e.target.style.background = 'black'
+  } else if (atpin == document.querySelector('#blue')) {
+    e.target.style.background = 'blue'
+  } else if (atpin == document.querySelector('#green')) {
+    e.target.style.background = 'green'
+  } else if (atpin == document.querySelector('#red')) {
+    e.target.style.background = 'red'
+  }
+  /* console.log(atpin) */
+  /* e.target.style.background = 'red' */
 }
 
 let pix2 = document.querySelectorAll('.pixel')
 for (i of pix2) {
-    i.addEventListener('click', colorir)
+  i.addEventListener('click', colorir)
 }
 
 function limpar() {
-    let pix3 = document.querySelectorAll('.pixel')
-    for (i of pix3) {
-        i.style.background = 'white'
-    }
+  let pix3 = document.querySelectorAll('.pixel')
+  for (i of pix3) {
+    i.style.background = 'white'
+  }
 }
-
-let btn = document.querySelector('#clear-board')
-btn.addEventListener('click', limpar)
+botao.addEventListener('click', limpar)
